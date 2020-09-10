9103c3f15a3683b849b5b18b1e4a6587ebbf4b22
Anatoli Belski <anatoli@bel.ski>
2020-09-10 22:49:00 +0200

    fix: Gallery lightbox index

--- src/Gallery/Gallery.jsx
+++ src/Gallery/Gallery.jsx
@@ -1,11 +1,21 @@
 /* eslint-disable react/button-has-type */
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import GalleryComponent from 'react-photo-gallery'
 import FsLightbox from 'fslightbox-react'
 
 function Gallery({ photos, update }) {
-  const [viewerIsOpen, setViewerIsOpen] = useState(false)
+  const [lightboxController, setLightboxController] = useState({
+    toggler: false,
+    slide: 1,
+  })
+
+  const toggleLightbox = useCallback((event, { photo, index }) => {
+    setLightboxController({
+      toggler: !lightboxController.toggler,
+      slide: index !== undefined || index !== null ? index + 1 : 1,
+    })
+  })
 
   const images =
     photos?.map(image => {
@@ -18,8 +28,8 @@ function Gallery({ photos, update }) {
 
   return (
     <div>
-      <GalleryComponent photos={images} onClick={() => setViewerIsOpen(!viewerIsOpen)} />
-      <FsLightbox toggler={viewerIsOpen} sources={photos} key={update} />
+      <GalleryComponent photos={images} onClick={toggleLightbox} />
+      <FsLightbox toggler={lightboxController.toggler} sources={photos} key={update} slide={lightboxController.slide} />
     </div>
   )
 }
